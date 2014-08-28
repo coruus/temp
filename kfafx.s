c48a700778526f6ab4987d7c55bce23dc31d5aad
David Leon Gil <coruus@gmail.com>
2014-08-27 23:42:47 -0400

    WIP

--- kfafx.s
+++ kfafx.s
@@ -1,6 +1,36 @@
 [CPU intelnop]
 [bits 64]
 
+[section .data]
+
+align 32
+
+__rho_00_03_l:
+dq 0, 1, 62, 28
+__rho_00_03_r:
+dq 64, 64-1, 64-62, 64-28
+
+__rho_05_08_l:
+dq 36, 44, 6, 55
+__rho_05_08_r:
+dq 64-36, 64-44, 64-6, 64-55
+
+__rho_10_13_l:
+dq 3, 10, 43, 25
+__rho_10_13_r:
+dq 64-3,64-10,64-43,64-25
+
+__rho_15_18_l:
+dq 41,45,15,21
+__rho_15_18_r:
+dq 64-41,64-45,64-15,64-21
+
+__rho_20_23_l:
+dq 18,2,61,56
+__rho_20_23_r:
+dq 64-18,64-2,64-61,64-56
+
+
 [section .text]
 
 align 32
@@ -30,9 +60,14 @@ _theta:
 %define y7 ymm7
 %define y8 ymm8
 %define y9 ymm9
+%define y10 ymm10
 
-vzeroall
+%define INTER 1
+
+vzeroupper
 rdtsc
+shl rdx, 32
+add rax, rdx
 mov r8, rax
 
 vmovdqu y0, state(0 ) ; y0 = { x[ 0], x[ 1], x[ 2], x[ 3] }
@@ -56,7 +91,7 @@ xor r1, state(19) ; r1'   = r1   ^ x[19]
 xor r0, state(24) ; r0''  = r0'  ^ x[24]
 xor r0, r1        ; r0''' = r0'' ^ r1'
 ;; r0 == C[4]
-mov Cs(4), r0     ; Cs(4) = r0 = C[4]
+mov Ds(0), r0     ; Cs(4) = r0 = C[4]
 
 ;; compute C[0..3]
 vpxor y0, y0, y1  ; y0'   = y0   ^ y1
@@ -65,40 +100,95 @@ vpxor y0, y0, y4  ; y0''  = y0'  ^ y4  == y0 ^ y1 ^ y4
 vpxor y0, y0, y2  ; y0''' = y0'' ^ y2' == y0 ^ y1 ^ y4 ^ y2 ^ y3
 ;; y0 == { C[0], C[1], C[2], C[3] }
 
-vmovdqu Cs(0 ), y0     ; Cs(0..3) = y0
-vmovdqu Cs(5 ), y0     ; Cs(5..8) = y0
-;; Cs = { C[0], C[1], C[2], C[3], C[4], C[0], C[1], C[2], C[3] }
-
-;; { C[4], C[0], C[1], C[2] ]
-;; { C[1], C[2], C[3], C[4] } <<< 1
-;; { D[0], D[1], D[2], D[3] }
-vmovdqu  y1, Cs(1)
-vmovdqu  y0, Cs(4)
-
-vpsllq  y2, y1, 1      ; y2   = y1 << 1
-vpsrlq  y1, y1, 63     ; y1'  = y1 >> 63
-vpxor   y1, y1, y2     ; y1'' = y1 ^ y2    =
-vpxor   y0, y0, y1     ; y0'  = y0 ^ y1    = { D[1], D[2], D[3], D[4] }
-vmovdqu Ds(0 ), y0
+;vmovdqu Ds(0), y0
+vpbroadcastq y3, Ds(0)
+;; y3 == { C[3], C[3], C[3], C[3] }
+;vmovdqu Cs(0), y0
+;; Cs[0..4] == C[0..4]
+;vpermq y1, y0, 11111001b
+;vpermq y0, y0, 10010000b
+vpermq y1, y0, 11111001b
+;; y1 == { C[1], C[2], C[3], ___ }
+vpermq y0, y0, 10010000b
+;; y0 == { ___,  C[0], C[1], C[2] }
+vpblendd y1, y1, y3, 11000000b
+;; y1 == { C[1], C[2], C[3], C[4] }
+vpblendd y0, y0, y3, 00000011b 
+;; y0 == { C[4], C[0], C[1], C[2] }
 
 rorx    r0, Cs(0 ), 63 ; r1   = C[1] <<< 1
 xor     r0, Cs(3 )     ; r1'  = r1 ^ C[4]  = D[0]
-mov     Ds(4 ), r0
-
-vpxor   y1, y0, y5
-vpxor   y2, y0, y6  
-vpxor   y3, y0, y7
-vpxor   y4, y0, y8 
-vpxor   y5, y0, y9
-
-xor     A(9),  r0
+;  mov     Ds(4 ), r0
+xor     A( 4), r0
+xor     A( 9), r0
 xor     A(14), r0
 xor     A(19), r0
 xor     A(24), r0
 
+rorx r0, A(4), 64-27
+mov A(4), r0
+rorx r1, A(9), 64-20
+mov A(9), r1
+rorx r0, A(14), 64-39
+mov A(14), r0
+rorx r1, A(19), 64-8
+mov A(19), r1
+rorx r0, A(24), 64-14
+mov A(24), r0
 
+vpsllq y2, y1, 1
+vpsrlq y1, y1, 63
+;; y2 == y1 <<< 1
+
+vpxor  y1, y1, y2
+vpxor  y0, y0, y1
+
+vpxor   y1, y0, y5  ;; y1 = A[0..3]
+vpxor   y2, y0, y6  ;; y2 = A[5..8] 
+vpxor   y3, y0, y7  ;; y3 = A[10..13]
+vpxor   y4, y0, y8  ;; y4 = A[15..18]
+vpxor   y5, y0, y9  ;; y5 = A[20..23]
+
+vpsllvq  y6, y1, [rel __rho_00_03_l]
+vpsrlvq  y1, y1, [rel __rho_00_03_r]
+
+vpsllvq  y7, y2, [rel __rho_05_08_l]
+vpsrlvq  y2, y2, [rel __rho_05_08_r]
+
+vpsllvq  y8, y3, [rel __rho_10_13_l]  
+vpsrlvq  y3, y3, [rel __rho_10_13_r] 
+
+vpsllvq  y9, y4, [rel __rho_15_18_l]
+vpsrlvq  y4, y4, [rel __rho_15_18_r]
+
+vpsllvq  y10, y5, [rel __rho_20_23_l]
+vpsrlvq  y5, y5,  [rel __rho_20_23_r]
+
+vpxor    y1, y1, y6
+vpxor    y2, y2, y7
+vpxor    y3, y3, y8
+vpxor    y4, y4, y9
+vpxor    y5, y5, y10
+
+
+
+  ;vmovdqu Ds(0), y0
+
+
+  ;vmovdqu Cs(0 ), y0     ; Cs(0..3) = y0
+  ;vmovdqu Cs(5 ), y1     ; Cs(5..8) = y0
+
+%if INTER
+  vmovdqu A(0), y1
+  vmovdqu A(5), y2
+  vmovdqu A(10), y3
+  vmovdqu A(15), y4
+  vmovdqu A(20), y5
+%endif
 
 rdtsc
+shl rdx, 32
+add rax, rdx
 sub rax, r8
 
 vzeroall
--- theta.c
+++ theta.c
@@ -3,6 +3,9 @@
 #include <stdio.h>
 #include <string.h>
 
+#define cycles __builtin_readcyclecounter
+
+
 typedef struct {
   uint64_t can0[4];
   uint64_t A[25];
@@ -43,6 +46,19 @@ static inline uint64_t rol(uint64_t x, uint8_t s) {
   return (x << s) ^ (x >> (64 - s));
 }
 
+static const uint8_t rhol[25] = { 
+  0, 1, 62, 28, 27,
+  36, 44, 6, 55, 20,
+  3, 10, 43, 25, 39,
+  41, 45, 15, 21, 8,
+  18, 2, 61, 56, 14 };
+
+void rho_ref(state* s) {
+  for (int i = 0; i < 25; i++) {
+    s->A[i] = rol(s->A[i], rhol[i]);
+  }
+}
+
 void theta_ref(state* s) {
   for (int x = 0; x < 5; x++) {
     s->C[x] = 0;
@@ -54,7 +70,14 @@ void theta_ref(state* s) {
     s->D[x] = s->C[(x+4)%5] ^ rol(s->C[(x+1)%5], 1);
   }
   //memcpy(&(s->Cp), &(s->C), 3 * 8);
+  for (int x = 0; x < 5; x++) {
+    for (int y = 0; y < 25; y += 5) {
+      s->A[x + y] ^= s->D[x];
+    }
+  }
 }
+
+extern void keccakf(void*);
 
 int main(void) {
   state s, sref;
@@ -66,13 +89,23 @@ int main(void) {
 
   printf("ref\n");
   theta_ref(&sref);
+  rho_ref(&sref);
   printstate(sref);
   printf("\nasm");
   uint64_t c = theta(&(s.A));
+  //kck(&(s.A));
   printstate(s);
 
+  uint64_t a[25] = {0};
+  uint64_t kscyc = cycles();
+  for (uint64_t i = 0; i < N; i++) {
+    keccakf(a);
+  }
+  kscyc = cycles() - kscyc;
+  double cpi = (double)kscyc / (double)N;
+  printf("\nk.s: %llu, %f, %f\n", kscyc, cpi, cpi / 200.0);
+
   printf("\ncycles=%llu\n", c);
-  return 0;
   double cycles = 0;
   uint64_t min = SIZE_MAX;
   for (uint64_t i = 0; i < N; i++) {
@@ -80,6 +113,7 @@ int main(void) {
     cycles += cyc;
     min = (cyc < min) ? cyc : min;
   }
-  printf("theta: avg=%f,min=%llu\n", cycles / (double)(N), min);
+  printf("theta: avg=%f,min=%llu\n,cpb=%f", 24 * cycles / (double)(N), 24 * min,
+      cycles / (double)N / 8.333333);
   return 0;
 }
